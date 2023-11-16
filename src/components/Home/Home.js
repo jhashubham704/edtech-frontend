88d5df1980e4dc576e839ed1a0c77daf4b0ef015
Himanshu Sharma <jalotrahimanshu13@gmail.com>
2023-11-16 19:24:46 +0530

    folder structure fromed

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Register from '../register/Register'
+import Contact from '../courses/Course'
+import Login from '../login/Login'
+import Nav from '../nav/Nav'
+import Note from '../notes/Note'
+
+
+
+
+export default function Home() {
+  return (
+    <div>
+      Home
+      </div>
+  )
+}
